8f62d2c9511f63e01eae3ad2a50915f0429e520c
Pawan lakhera <pawanlakhera77@gmail.com>
2020-10-11 03:29:12 +0530

    Updates

---
+++ precache-manifest.eacc18aa0173bb258275f0f130ccc406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251484ea6716225e5bc4e691f39b4c84",
+    "url": "/twilio-video-react-hooks/index.html"
+  },
+  {
+    "revision": "37c64319fd95ee804cf9",
+    "url": "/twilio-video-react-hooks/static/css/main.3fdacb61.chunk.css"
+  },
+  {
+    "revision": "da5f2f52093375022c8a",
+    "url": "/twilio-video-react-hooks/static/js/2.a4e9a941.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/twilio-video-react-hooks/static/js/2.a4e9a941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c64319fd95ee804cf9",
+    "url": "/twilio-video-react-hooks/static/js/main.a0403ece.chunk.js"
+  },
+  {
+    "revision": "a3925637c65ece8c31d1",
+    "url": "/twilio-video-react-hooks/static/js/runtime-main.4f9c2966.js"
+  }
+]);
